1d22edab79c42a7796bc7cb00fe0ed8491a5e525
Joan Heredia Rius <watxaut@gmail.com>
2020-01-07 12:05:12 +0100

    changed folder name src->gimpify, package uploaded to PyPI

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 import setuptools
 
+from gimpify.__init__ import __version__
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gimpify",
-    version="0.0.1",
+    name="gimpify-watxaut",
+    version=__version__,
     author="watxaut",
     author_email="watxaut@gmail.com",
     description="Just a package to create automatic face image montages",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/watxaut/gimpify",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="."),
+    keywords="face recognition montage",
+    # py_modules=['gimpify', 'gimpify.montages'],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    project_urls={
+        "Source": "https://github.com/watxaut/gimpify",
+        "Tracker": "https://github.com/watxaut/gimpify/issues",
+    },
+    install_requires=["face-recognition>=1.2.3", "Pillow>=7.0.0"],
 )
